b280b4bd659b62057f7c95cfeba76cabb6c59ade
Daniel Lyons <fusion@storytotell.org>
2012-06-07 08:31:25 -0600

    this method in the example file is confusing haddock, so it must be commented out for now

--- src/Automata.hs
+++ src/Automata.hs
@@ -12,6 +12,6 @@ instance Automaton Machine where
   evaluateMachine (DFAMachine d) = evaluateMachine d
   evaluateMachine (Other) = error "hi"
   
-  parseMachine s = case (parseMachine s :: Either String DFA) of
-    Left _ -> Right Other
-    Right dfa -> Right $ DFAMachine dfa
+--  parseMachine s = case (parseMachine s :: Either String DFA) of
+--    Left _ -> Right Other
+--    Right dfa -> Right $ DFAMachine dfa
